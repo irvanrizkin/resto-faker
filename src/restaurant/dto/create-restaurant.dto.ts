ca6bce0cc8ddf86c2c3d578736c340d988596c33
irvanrizkin <irvanriskinugraha90@gmail.com>
2023-11-15 20:46:40 +0700

    domain restaurant

---
+++ src/restaurant/dto/create-restaurant.dto.ts
@@ -0,0 +1,5 @@
+export class CreateRestaurantDto {
+  name: string;
+  price: number;
+  status: boolean;
+}
---
+++ src/restaurant/restaurant.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { randomBytes } from 'crypto';
+
+@Injectable()
+export class RestaurantService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createRestaurantDto: CreateRestaurantDto) {
+    const id = `RES${randomBytes(5).toString('hex')}`;
+    return await this.prisma.restaurant.create({
+      data: {
+        id,
+        ...createRestaurantDto,
+      },
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.restaurant.findMany();
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.restaurant.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: string, updateRestaurantDto: UpdateRestaurantDto) {
+    return await this.prisma.restaurant.update({
+      where: {
+        id,
+      },
+      data: updateRestaurantDto,
+    });
+  }
+
+  async remove(id: string) {
+    return await this.prisma.restaurant.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
